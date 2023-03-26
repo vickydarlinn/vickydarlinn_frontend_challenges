89822584e5c93908025031a8e713a76293570404
vickydarlinn <uttapalsangwan@gmail.com>
2023-03-26 05:58:44 +0530

    feat: theme modes added

--- github_user_search_app/src/App.js
+++ github_user_search_app/src/App.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
-import "./index.css";
+import React, { useState, useEffect, useContext } from "react";
 import SearchBar from "./components/SearchBar";
 import UserCard from "./components/UserCard";
 import Header from "./components/Header";
+import ThemeContext from "./context/ThemeContext";
+import "./index.css";
 
 const App = () => {
   const [query, setQuery] = useState("");
   const [data, setData] = useState("");
   const [err, setErr] = useState(false);
-  const [lightMode, setLightMode] = useState("light");
+  // const [lightMode, setLightMode] = useState("light");
+  const { lightMode, modeChanger } = useContext(ThemeContext);
+
   useEffect(() => {
     if (query !== "") {
       getGithubUserData();
@@ -26,13 +29,12 @@ const App = () => {
   }
 
   return (
-    <div
-      className="app"
-      data-theme={`${lightMode === "light" ? "light" : "dark"}`}
-    >
-      <Header />
-      <SearchBar setQuery={setQuery} err={err} />
-      <UserCard data={data} />
+    <div className="app" data-theme={`${lightMode ? "dark" : null}`}>
+      <div className="container">
+        <Header />
+        <SearchBar setQuery={setQuery} err={err} />
+        <UserCard data={data} />
+      </div>
     </div>
   );
 };
--- github_user_search_app/src/components/Header.js
+++ github_user_search_app/src/components/Header.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BsMoonFill } from "react-icons/bs";
+import React, { useContext } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import ThemeContext from "../context/ThemeContext";
 
 const Header = () => {
+  const { lightMode, modeChanger } = useContext(ThemeContext);
   return (
     <header>
       <div className="logo">devfinder</div>
-      <div className="themeMode">
-        <i>
-          <BsMoonFill />
-        </i>{" "}
-        DARK
+      <div className="themeMode" onClick={modeChanger}>
+        <i>{lightMode ? <BsMoonFill /> : <BsSunFill />}</i>
+        {lightMode ? "Dark" : "Light"}
       </div>
     </header>
   );
